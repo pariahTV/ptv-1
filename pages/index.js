e9b3f747da994dc8ae44a68fc1f65d11ea81f565
pariahTV <pariahfm@gmail.com>
2026-06-17 20:39:28 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{backgroundColor:'#000',color:'#fff',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <h1>PariahTV Viewer is live</h1>
+    </div>
+  );
+}
